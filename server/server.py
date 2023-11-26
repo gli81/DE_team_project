4928a49b35c7854c5867d2e330f9f3c48fe58a2d
LGZ <liguangzheng998@hotmail.com>
2023-11-25 23:58:42 -0500

    added react

--- server/server.py
+++ server/server.py
@@ -3,8 +3,17 @@
 ## do CRUD now, include flask later
 
 
-from databricks import sql
-import pandas as pd
-from dotenv import load_dotenv
-import os
+# from databricks import sql
+# import pandas as pd
+# from dotenv import load_dotenv
+# import os
+from flask import Flask
 
+app = Flask(__name__)
+
+# routing 
+
+@app.route("/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
