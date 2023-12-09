9c43c54d9a89beb6244bb67580ae8def02862682
xueqingw@bu.edu <xueqingw@bu.edu>
2023-12-09 03:12:09 +0000

    databricks ETL

---
+++ server/mylib/transform_load_databricks.py
@@ -0,0 +1,26 @@
+"""
+transform and load function
+"""
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import monotonically_increasing_id
+
+def load(dataset="dbfs:/FileStore/data/food_new.csv"):
+    spark = SparkSession.builder.appName("Read CSV").getOrCreate()
+    # load csv and transform it by inferring schema 
+    food = spark.read.csv(dataset, header=True, inferSchema=True)
+
+    # add unique IDs to the DataFrames
+    food = food.withColumn("id", monotonically_increasing_id())
+
+    # transform into a delta lakes table and store it 
+    food.write.format("delta").mode("overwrite").saveAsTable("Food")
+
+    
+    num_rows = food.count()
+    print(num_rows)
+    
+    return "finished transform and load"
+
+if __name__ == "__main__":
+    load()
